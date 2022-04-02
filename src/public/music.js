bffe572598e22427c909a78629c4196325913bac
Chrissie Zimmer <czimmer12@aol.com>
2022-04-02 19:45:07 -0400

    music

--- src/public/music.js
+++ src/public/music.js
@@ -1,6 +1,14 @@
 let songs = [
-    {title : "call", path : "./audio/call.mp3"},
-    {title : "Homework", path : "Homework.mp3"},
+    {title : "cali", path : "./audio/cali.mp3"},
+    {title : "Homework", path : "./audio/Homework.mp3"},
+    {title : "Hope", path : "./audio/Hope.mp3"},
+    {title : "Ice", path : "./audio/Ice.mp3"},
+    {title : "NightLight", path : "./audio/NightLight.mp3"},
+    {title : "Pride", path : "./audio/Pride.mp3"},
+    {title : "Pursuit", path : "./audio/Pursuit.mp3"},
+    {title : "Thing", path : "./audio/Thing.mp3"},
+    {title : "Trippin", path : "./audio/Trippin.mp3"},
+    {title : "Warrior", path : "./audio/Warrior.mp3"},
 ]
 
 
