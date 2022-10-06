5fe029674c5cf57f9a9f75330d754ab25ba58864
amoahn86 <114532414+amoahn86@users.noreply.github.com>
2022-10-06 12:52:30 -0400

    Added old code from cpstnePrjt

--- src/public/spotify.js
+++ src/public/spotify.js
@@ -1,1 +1,22 @@
-console.log("YOU ARE WORKING HERE")
+//Linked to Music.html page
+
+
+//let userAccessToken = ;
+//let beats = ;
+fetch("https://api.spotify.com/v1/audio-analysis/6EJiVf7U0p1BBfs0qqeb1f", {
+  method: "GET",
+  headers: {
+    Authorization: `Bearer ${userAccessToken}`
+  }
+})
+.then(response => response.json())
+.then(({beats}) => {
+  
+    beats.forEach((beat, index) => {
+    console.log(`Beat ${index} starts at ${beat.start}`);
+  })
+})
+
+
+
+//stringify the data and print it to html
