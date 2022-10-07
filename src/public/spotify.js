58e6baf36a72b891581f4368d42ff592207ff495
Chrissie Zimmer <czimmer12@aol.com>
2022-10-06 23:34:42 -0400

    js

--- src/public/spotify.js
+++ src/public/spotify.js
@@ -3,13 +3,7 @@
 
 //let userAccessToken = ;
 //let beats = ;
-var http = require('http');
-http
-    .createServer(function(request, response){
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.end();
-    })
-    .lis
+
 
 
 //stringify the data and print it to html
