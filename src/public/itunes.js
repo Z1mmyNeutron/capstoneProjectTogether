0944c56de03da873c236c7140260769757d079a3
Chrissie Zimmer <czimmer12@aol.com>
2022-10-10 18:48:16 -0400

    itunes with errors

--- src/public/itunes.js
+++ src/public/itunes.js
@@ -1,25 +1,36 @@
 window.onload =function(){
-    const url = 'https://itunes.apple.com/search?term=summer&country=us&entity=song&limit=25';
-    const url2 = 'https://itunes.apple.com/search?country=us&entity=song&limit=25';
-
     let input = document.getElementById('search-input-itunes');
     let dataTag = document.getElementById('data-holder-itunes');
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("country", "us");
+    encodedParams.append("term", input);
+    encodedParams.append("limit", "25");
 
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'X-RapidAPI-Key': '4c187f9309msha2277d1a56df93fp1c9f85jsn2b8dafccf3ff',
+            'X-RapidAPI-Host': 'iTunesvolodimir-kudriachenkoV1.p.rapidapi.com'
+        },
+        body: encodedParams
+    };
+    let dataHolder = [];
 
-    document.getElementById('search-itunes-button').addEventListener('click', (e) => { 
-        fetch(url)
-        .then((res) => {
-            console.log(res.status)
-            return res.json()
+    document.getElementById('search-itunes').addEventListener('click', (e) => { 
+        e.preventDefault();
+        let url = 'https://itunesvolodimir-kudriachenkov1.p.rapidapi.com/searchMusic'
+        
+        fetch(url, options)
+        .then(response => response.json())
+        .then(data => {
+                console.log(data.results);
+                const ameriSongs = data.results;
+                dataHolder.push(ameriSongs)
+                let s = JSON.stringify(dataHolder, null, "\t")
+                dataTag.innerHTML = s
+
+            })
+            .catch(err => console.error(err));
         })
-        .then((data) => {
-            console.log(data.results);
-            const ameriSongs = data.results;
-            dataHolder.push(ameriSongs)
-            let s = JSON.stringify(dataHolder, null, "\t")
-            dataTag.innerHTML = s
-
-        })
-        .catch(err => console.error(err));
-    })
 }
