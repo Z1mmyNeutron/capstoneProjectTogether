b628d41f0cd522c600149b1f048be25e17426e4a
Zach <z.nealis@gmail.com>
2022-10-12 14:58:56 -0400

    test

--- src/public/deeZer.js
+++ src/public/deeZer.js
@@ -10,14 +10,11 @@ let dataHolder = [];
 document.getElementById('search-dee').addEventListener('click', (e) => { 
     e.preventDefault();
 
-        DZ.api('/search?q=' + input.value, function(response){
-            then(data => { dataHolder.push(data)
-                let x = JSON.stringify(data, null, 4)
-                dataTag.innerHTML = x
-            console.log("DataHolder", response.data);
-
+    function search(term){
+        DZ.api('/search?q=' + term, function(response){
+            console.log(response.data);
         });
-    })
+    }
 })
 }
 
