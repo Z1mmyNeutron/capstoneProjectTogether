ca3a675eb247becf7f3edf7de44d2b0856a4b288
Zach <z.nealis@gmail.com>
2022-10-12 13:59:10 -0400

    test

--- src/public/deeZer.js
+++ src/public/deeZer.js
@@ -3,14 +3,6 @@
 window.onload = function(){
 	let input = document.getElementById('search-input-dee');
 	let dataTag = document.getElementById('data-holder-dee');
-    
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '4c187f9309msha2277d1a56df93fp1c9f85jsn2b8dafccf3ff',
-            'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-        }
-    };
 
 
 
@@ -18,16 +10,17 @@ let dataHolder = [];
 document.getElementById('search-dee').addEventListener('click', (e) => { 
     e.preventDefault();
 
-    let url = `https://deezerdevs-deezer.p.rapidapi.com/search?q=${input.value}`;
-
-fetch(url, options)
-    .then(response => response.json())
-    .then(data => {
-        dataHolder.push(data)
-        let x = JSON.stringify(data, null, 4)
-        dataTag.innerHTML = x
-    })
-    .catch(err => console.error(err));
+    function search(term){
+        input = term
+        DZ.api('/search?q=' + input, function(response){
+            then(response => response.json())
+            .then(data => { dataHolder.push(data)
+            let x = JSON.stringify(data, null, 4)
+            dataTag.innerHTML = x  
+            })
+            .catch(err => console.error(err))
+            console.log("DataHolder", dataHolder);
+        });
+    }
 })
-console.log("DataHolder", dataHolder)
 }
